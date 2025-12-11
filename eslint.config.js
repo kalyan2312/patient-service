662099359bdfb3a6435c2e3f32d83488d340479b
Kalyan appari <111048792+kalyan2312@users.noreply.github.com>
2025-12-11 17:58:39 +0530

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+// eslint.config.js
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+	// matches all files ending with .js
+	{
+		files: ["**/*.js"],
+		rules: {
+			semi: "error",
+		},
+	},
+
+	// matches all files ending with .js except those in __tests
+	{
+		files: ["**/*.js"],
+		ignores: ["__tests/**"],
+		rules: {
+			"no-console": "error",
+		},
+	},
+]);
